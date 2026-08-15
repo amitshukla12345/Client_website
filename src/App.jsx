670efdd4b0b52365b8c287a17a508807b0ef4f04
Amit Shukla <amitshukla22509@gmail.com>
2026-08-16 00:46:28 +0530

    buil,deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import { AppProvider } from './context/AppContext'
 import { LanguageProvider } from './context/LanguageContext'
 import AppRoutes from './routes/Index'
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/katha-website-demo/',
   plugins: [react()],
 })
